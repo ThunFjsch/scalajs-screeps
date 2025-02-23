7c46b06ffbebb25c06a39c0c7d7e5d5b5fd7c071
ThunFjsch <arinartz4@gmail.com>
2025-02-23 12:32:32 +0100

    Updated Grunt/config for private server

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,15 @@ module.exports = function(grunt) {
     grunt.initConfig({
         screeps: {
             options: {
-                email: config.email,
-                password: config.password,
+                email: config.user.email,
+                password: config.user.password,
+                token: config.user.token,
                 branch: 'default',
-                //server: 'season'
+                server: {
+                    host: config.server.host,
+                    port: config.server.port,
+                    http: config.server.http
+                },
             },
             dist: {
                 src: ['target/scala-2.13/scala-example-bot-fastopt/main.js']
